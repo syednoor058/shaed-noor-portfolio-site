d77fcad871edd00c2fbc1c4aa70ad2d578c89334
syednoor058 <syednoor058@gmail.com>
2024-07-02 12:14:51 +0600

    Error messages design changed.

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,4 +1,5 @@
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useRef, useState } from "react";
 import { FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa6";
@@ -11,6 +12,10 @@ export default function Contact() {
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
+  const [nameErrMsg, setNameErrMsg] = useState("");
+  const [phoneErrMsg, setPhoneErrMsg] = useState("");
+  const [emailErrMsg, setEmailErrMsg] = useState("");
+  const [subErrMsg, setSubErrMsg] = useState("");
   const [errMsg, setErrMsg] = useState("");
   const [successMsg, setSuccessMsg] = useState("");
 
@@ -23,63 +28,94 @@ export default function Contact() {
 
   const form = useRef();
 
+  const closeSuccessMsg = (e) => {
+    e.preventDefault();
+    setSuccessMsg("");
+  }
+
   const handleSend = (e) => {
     e.preventDefault();
 
     if (username === "") {
-      setErrMsg("Name is required!");
-    } else if (phoneNumber === "") {
-      setErrMsg("Phone number is required!");
-    } else if (email === "") {
-      setErrMsg("Email is required!");
-    } else if (!emailValidation(email)) {
-      setErrMsg("Invalid email address!");
-    } else if (subject === "") {
-      setErrMsg("Subject is required!");
-    } else if (message === "") {
-      setErrMsg("Empty message can not be sent!");
+      setNameErrMsg("* Name is required!");
     } else {
-      setSuccessMsg(
-        `Thank you dear ${username}, Your Messages has been sent Successfully!`
-      );
-
-      emailjs
-        .sendForm("service_k918xce", "template_bwke4fq", form.current, {
-          publicKey: "RVdjFgeK3-m4-E8RM",
-        })
-        .then(
-          () => {
-            console.log("SUCCESS!");
-          },
-          (error) => {
-            console.log("FAILED...", error.text);
+      setNameErrMsg("");
+      if (phoneNumber === "") {
+        setPhoneErrMsg("* Phone number is required!");
+      } else {
+          setPhoneErrMsg("");
+          if (email === "") {
+            setEmailErrMsg("* Email is required!");
+          } else if (!emailValidation(email)) {
+              setEmailErrMsg("* Invalid email address!");
+          } else {
+            setEmailErrMsg("");
+            if (subject === "") {
+              setSubErrMsg("* Subject is required!");
+            } else {
+              setSubErrMsg("");
+              if (message === "") {
+                setErrMsg("* Blank message can not be sent!");
+              }
+              else {
+                setErrMsg("");
+                setSuccessMsg(
+                  `Thank you dear ${username}, your message has been sent successfully!`
+                );
+          
+                emailjs
+                  .sendForm("service_k918xce", "template_bwke4fq", form.current, {
+                    publicKey: "RVdjFgeK3-m4-E8RM",
+                  })
+                  .then(
+                    () => {
+                      console.log("SUCCESS!");
+                    },
+                    (error) => {
+                      console.log("FAILED...", error.text);
+                    }
+                  );
+                  setErrMsg("");
+                  setUsername("");
+                  setPhoneNumber("");
+                  setEmail("");
+                  setSubject("");
+                  setMessage("");
+                  setNameErrMsg("");
+                  setPhoneErrMsg("");
+                  setEmailErrMsg("");
+                  setSubErrMsg("");
+              }
+            }
           }
-        );
-
-      setErrMsg("");
-      setUsername("");
-      setPhoneNumber("");
-      setEmail("");
-      setSubject("");
-      setMessage("");
+      }
     }
-  };
+  }
 
   return (
     <section id="contact" className="w-full pt-10 lg:pt-0 pb-10 md:pb-20">
       <div className="flex justify-center items-center text-center">
         <div className="flex flex-col gap-4 font-titleFont mb-14">
-          <h3 className="text-sm uppercase font-light text-designColor tracking-wide" data-aos="zoom-in">
+          <h3
+            className="text-sm uppercase font-light text-designColor tracking-wide"
+            data-aos="zoom-in"
+          >
             Contact
           </h3>
-          <h1 className="text-2xl md:text-3xl text-bodyColor font-bold capitalize" data-aos="zoom-in">
+          <h1
+            className="text-2xl md:text-3xl text-bodyColor font-bold capitalize"
+            data-aos="zoom-in"
+          >
             Let's have a conversation
           </h1>
         </div>
       </div>
       <div className="w-full">
         <div className="w-full h-full flex flex-col md:flex-row gap-10 lg:gap-16 justify-center lg:p-5 items-center">
-          <div className="w-[85%] lg:w-[30%] xl:w-[30%] h-full" data-aos="zoom-in">
+          <div
+            className="w-[85%] lg:w-[30%] xl:w-[30%] h-full"
+            data-aos="zoom-in"
+          >
             <div className="bg-cardColor shadow-shadowTwo p-5 lg:p-8 flex flex-col gap-5 lg:gap-8 rounded-md">
               <div className="w-full flex justify-center">
                 <img
@@ -150,78 +186,118 @@ export default function Contact() {
               </div>
             </div>
           </div>
-          <div className="w-full lg:w-[50%] xl:w-[50%] h-full" data-aos="zoom-in">
+          <div
+            className="w-full lg:w-[50%] xl:w-[50%] h-full"
+            data-aos="zoom-in"
+          >
             <div className="w-full h-full">
               <div className="w-full h-full py-5 lg:py-0 lg:pb-8 px-5 lg:px-8 flex flex-col justify-center gap-8">
-                <h3 className='text-lg lg:text-xl text-designColor text-center text-wrap uppercase'>Send Me message!</h3>
-                <form ref={form} className="w-full flex flex-col gap-6 lg:gap-10">               
+                <h3 className="text-lg lg:text-xl text-designColor text-center text-wrap uppercase">
+                  Send Me message!
+                </h3>
+                <form
+                  ref={form}
+                  className="w-full flex flex-col gap-6 lg:gap-10"
+                >
                   <div className="w-full flex flex-col lg:flex-row gap-6 lg:gap-8">
                     <div className="w-full lg:w-1/2">
                       <input
                         onChange={(e) => setUsername(e.target.value)}
                         value={username}
-                        name='from_name'
-                        className='contactInput'
-                        placeholder='Enter full name'
+                        name="from_name"
+                        className="contactInput"
+                        placeholder="Enter full name"
                         type="text"
                       />
+                      {nameErrMsg && (
+                        <p className="pt-1 text-red-500 text-base tracking-wide">
+                          {nameErrMsg}
+                        </p>
+                      )}
                     </div>
                     <div className="w-full lg:w-1/2">
                       <input
                         onChange={(e) => setPhoneNumber(e.target.value)}
                         value={phoneNumber}
-                        name='from_mobile'
-                        className='contactInput'
-                        placeholder='Enter phone number'
+                        name="from_mobile"
+                        className="contactInput"
+                        placeholder="Enter phone number"
                         type="text"
                       />
+                      {phoneErrMsg && (
+                        <p className="pt-1 text-red-500 text-base tracking-wide">
+                          {phoneErrMsg}
+                        </p>
+                      )}
                     </div>
                   </div>
                   <div className="w-full">
                     <input
                       onChange={(e) => setEmail(e.target.value)}
                       value={email}
-                      name='from_email'
-                      className='contactInput'
-                      placeholder='Enter e-mail address'
+                      name="from_email"
+                      className="contactInput"
+                      placeholder="Enter e-mail address"
                       type="email"
                     />
+                    {emailErrMsg && (
+                        <p className="pt-1 text-red-500 text-base tracking-wide">
+                          {emailErrMsg}
+                        </p>
+                      )}
                   </div>
                   <div className="w-full">
                     <input
                       onChange={(e) => setSubject(e.target.value)}
                       value={subject}
-                      name='from_subject'
-                      className='contactInput'
-                      placeholder='Enter message subject'
+                      name="from_subject"
+                      className="contactInput"
+                      placeholder="Enter message subject"
                       type="text"
                     />
+                    {subErrMsg && (
+                        <p className="pt-1 text-red-500 text-base tracking-wide">
+                          {subErrMsg}
+                        </p>
+                      )}
                   </div>
                   <div className="w-full">
                     <textarea
                       onChange={(e) => setMessage(e.target.value)}
                       value={message}
-                      name='message'
-                      className='contactTextArea'
-                      placeholder='Write message here'
+                      name="message"
+                      className="contactTextArea"
+                      placeholder="Write message here"
                       type="text"
                       cols="lg:30"
                       rows="6"
                     ></textarea>
+                    {errMsg && (
+                        <p className="pt-1 text-red-500 text-base tracking-wide">
+                          {errMsg}
+                        </p>
+                      )}
                   </div>
 
-                  {errMsg && (
-                    <p className="py-1 lg:py-3 bg-cardColor text-center text-orange-500 text-base tracking-wide animate-bounce">
-                      {errMsg}
-                    </p>
-                  )}
-                  {successMsg && (
-                    <p className="py-3 bg-cardColor text-center text-green-500 text-base tracking-wide animate-bounce">
-                      {successMsg}
-                    </p>
-                  )}
-
                   <div className="w-full">
+                    <AnimatePresence>
+                        {successMsg && (
+                          <motion.div initial={{ x: "-50%", opacity: 0}}
+                          animate={{ x: 0, opacity: 1, transition: { duration: 0.5 } }}
+                          exit={{ x: "-50%", opacity: 0, transition: { duration: 0.5 } }}
+                          className="w-full mb-2 py-2 px-3 rounded-sm bg-green-500 text-white text-base tracking-wide flex justify-between z-50">
+                            {successMsg}
+                            <span><button className="cursor-pointer text-bodyColor" onClick={closeSuccessMsg}>X</button></span>
+                          </motion.div>
+                        )}
+                      </AnimatePresence>
+                    {/* {successMsg && (
+                      <p className="mb-2 py-2 px-3 rounded-sm bg-green-500 text-white text-base tracking-wide flex justify-between">
+                        {successMsg}
+                        <span><button className="cursor-pointer text-bodyColor" onClick={closeSuccessMsg}>X</button></span>
+                      </p>
+                      
+                    )} */}
                     <button
                       onClick={handleSend}
                       className="w-full h-10 lg:h-12 bg-cardColorHover rounded-md text-sm lg:text-base text-gray-400 tracking-wider uppercase hover:text-white hover:bg-designColor duration-300 flex gap-2 justify-center items-center"
