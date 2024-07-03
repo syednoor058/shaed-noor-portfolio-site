5f9fa46aa8447dcef5b054bbe96e393ec0222c86
syednoor058 <syednoor058@gmail.com>
2024-07-03 16:43:06 +0600

    Shadow on light bg added (4).

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -116,7 +116,7 @@ export default function Contact() {
             className="w-[85%] lg:w-[30%] xl:w-[30%] h-full"
             data-aos="zoom-in"
           >
-            <div className="bg-cardColor shadow-xl shadow shadow-designColor p-5 lg:p-8 flex flex-col gap-5 lg:gap-8 rounded-md">
+            <div className="bg-cardColor shadow-xl shadow shadow-[#696969] p-5 lg:p-8 flex flex-col gap-5 lg:gap-8 rounded-md">
               <div className="w-full flex justify-center">
                 <img
                   className="h-[180px] lg:h-[250px] rounded-lg object-cover"
